da8745d86b5d3b59fe5344e58da092b36df1ec2d
mhrnbzrafkn <mhrn.bzrafkn@gmail.com>
2023-05-14 01:50:44 -0700

    add image binary effect completed.

---
+++ src/CNNTest/Form3.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace CNNTest
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 265);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(478, 44);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(256, 265);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(315, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Histogram Normalization";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(438, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = ">>";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(478, 315);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(256, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(53, 315);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(256, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Value = 127;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(315, 323);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "127";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Button button1;
+        private Label label1;
+        private ProgressBar progressBar1;
+        private TrackBar trackBar1;
+        private Label label2;
+    }
+}
---
+++ src/CNNTest/Form3.cs
@@ -0,0 +1,94 @@
+﻿using CSharpNet;
+using System.Security;
+
+namespace CNNTest
+{
+    public partial class Form3 : Form
+    {
+        private OpenFileDialog _openFileDialog;
+        private ImageProcessor _cNet;
+        public Bitmap _originalImage { get; private set; }
+        private int[,] _matrix;
+
+        public Form3()
+        {
+            InitializeComponent();
+            _cNet = new ImageProcessor();
+            _openFileDialog = new OpenFileDialog()
+            {
+                Filter = "Images files (*.jpg,*.png)|*.jpg;*.png|(*.jpg)|*.jpg|(*.png)|*.png",
+            };
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (_openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    _originalImage = new Bitmap(_openFileDialog.FileName);
+
+                    _matrix = _cNet.ConvertToGrayScaleMatrix(_originalImage);
+
+                    pictureBox1.Image = _originalImage;
+                    pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await ImageProcess();
+        }
+
+        private async void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label2.Text = trackBar1.Value.ToString();
+
+            await ImageProcess();
+        }
+
+        private async Task<int> ImageProcess()
+        {
+            progressBar1.Value = 0;
+            _matrix = _cNet.ConvertToGrayScaleMatrix(_originalImage);
+
+            var w = _matrix!.GetLength(0);
+            var h = _matrix.GetLength(1);
+
+            var edgeValue = trackBar1.Value;
+
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    var pixelValue = _matrix[i, j];
+                    if (pixelValue > edgeValue)
+                    {
+                        _matrix[i, j] = 255;
+                    }
+
+                    else
+                    {
+                        _matrix[i, j] = 0;
+                    }
+                }
+
+                var p = ((float)i / (float)w) * 100;
+                var z = (int)p;
+                progressBar1.Value = z;
+            }
+
+            var outputImage = _cNet.ConvertToBitmap(_matrix);
+            pictureBox2.Image = outputImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+            return await Task.FromResult(1);
+        }
+    }
+}
